ea6080bca436ee82700edecbfade5932fb3e94fe
deepy1524 <126562180+deepy1524@users.noreply.github.com>
2023-02-28 15:02:20 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+let mongoose=require("mongoose");
+mongoose.set("strictQuery", false);
+let connection=mongoose.connect(process.env.MONGO_URL);
+module.exports = {connection}
