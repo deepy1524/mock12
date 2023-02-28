9ac17f9f553359ca860b53646a11ae38db0f48bc
deepy1524 <126562180+deepy1524@users.noreply.github.com>
2023-02-28 15:02:40 +0530

    Add files via upload

---
+++ model/post.model.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const postSchema=mongoose.Schema({
+    name: String ,  
+    description:String,
+    category:String,
+    image:String,
+    location:String,
+    date:String,
+    price:Number
+})
+const PostModel=mongoose.model("post",postSchema);
+
+module.exports=PostModel;
